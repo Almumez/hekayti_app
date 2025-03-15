ecb72ddd984fee85b65220e8d0b299294c4c9e46
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2025-03-15 13:37:46 +0300

    feat(project):init project

---
+++ README.md
@@ -0,0 +1,43 @@
+# Hakaity App
+Application of children's stories to teach children to read
+
+
+
+## Overview
+The application contains the feature of listening to the story, as well as verifying the accuracy of the reading to the child. It also contains reports on the extent of progress.
+Some other features are as following:
+- Login / Signup .
+- The child can review the story.
+- Search for any story.
+- The child can listen to the story.
+- The child can record his voice while reading the story.
+- The application verifies that the child is reading the story correctly.
+- One of parants can change the settings.
+- View detailed reports and statistics about stories.
+- View progress.
+
+
+
+
+##  Screenshots
+
+   https://github.com/MaryamHajeb/Graduation-Project/tree/main/Screenshots
+
+## Project Setup
+
+### Clone and install
+
+Clone this repository and import into Android Studio
+```
+git clone https://github.com/MaryamHajeb/Graduation-Project.git
+```
+
+
+## Built With
+- Flutter
+- Bloc State Management
+- Sqlflite
+- google_speech package 
+- Google Speech to text
+---
+
---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:hikayati_app/core/AppTheme.dart';
+import 'package:hikayati_app/injection_container.dart' as object;
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'core/util/DataBaseHelper.dart';
+import 'dataProviders/network/Network_info.dart';
+import 'features/Introdection/presintation/page/SplashScreen.dart';
+import 'injection_container.dart';
+
+DataBaseHelper db = new DataBaseHelper();
+NetworkInfo networkInfo = NetworkInfoImpl(sl());
+String path = '';
+bool islogin = false;
+int idfrochart = 0;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlameAudio.bgm.initialize();
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.immersiveSticky,
+  );
+
+  final prefs = await SharedPreferences.getInstance();
+  islogin = await prefs.getBool('onbording') ?? false;
+
+  await object.init();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    //  DeviceOrientation.landscapeRight,
+  ]);
+  final Directory? dir = await getApplicationDocumentsDirectory();
+  final externalDirectoryPath = '${dir!.path}';
+  print(externalDirectoryPath);
+  path = externalDirectoryPath.toString();
+  await initEasyLoading();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        builder: EasyLoading.init(),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          shadowColor: AppTheme.primaryColor,
+
+          colorSchemeSeed: AppTheme.primaryColor,
+          useMaterial3: true,
+        ),
+        home: SplashScreen());
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    FlameAudio.bgm.pause();
+  }
+}
+
+initEasyLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.circle
+    ..indicatorWidget = SpinKitWaveSpinner(
+      color: AppTheme.primarySwatch.shade800,
+    )
+    ..loadingStyle = EasyLoadingStyle.light
+    ..indicatorSize = 80.0
+    ..radius = 10.0
+    ..progressColor = AppTheme.primaryColor
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.yellow
+    ..maskColor = Colors.black.withOpacity(0.5)
+    ..maskType = EasyLoadingMaskType.black
+    ..errorWidget = const Icon(
+      Icons.error,
+      size: 45,
+      color: Colors.red,
+    )
+    ..animationStyle = EasyLoadingAnimationStyle.opacity
+    ..infoWidget = const Icon(
+      Icons.info,
+      size: 45,
+      color: AppTheme.primaryColor,
+    )
+    ..successWidget = const Icon(
+      Icons.check_circle,
+      size: 45,
+      color: AppTheme.primaryColor,
+    )
+    ..dismissOnTap = false;
+}
